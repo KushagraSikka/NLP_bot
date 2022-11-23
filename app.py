811b7ecface9d3a0689f501cb618cdb351a3b6f7
Kushagra Sikka <kushagrasikka@gmail.com>
2022-11-23 22:40:50 +0530

    hey

---
+++ app.py
@@ -0,0 +1,19 @@
+from chat1 import chatbot_response
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.static_folder = 'static'
+
+@app.route("/")
+def home():
+    return render_template("index1.html")
+
+@app.route("/get")
+def get_bot_response():
+    UserTxt=request.args.get('msg')
+    resp=chatbot_response(UserTxt)
+    return str(resp)
+   
+
+if __name__ == "__main__":
+    app.run()
